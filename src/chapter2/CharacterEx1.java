0fad1829c4fe39c5bbb944d41d2db66c24e40c67
whitebear05 <woojs0505@naver.com>
2021-10-25 23:42:40 +0900

    Add: Variables and Characters

---
+++ src/chapter2/CharacterEx1.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class CharacterEx1 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println(ch1);        // 문자 출력
+        System.out.println((int)ch1);   // 문자에 해당하는 정수 값(아스키 코드 값) 출력
+
+        char ch2 = 66;                  // 정수 값 대입
+        System.out.println(ch2);        // 정수 값에 해당하는 문자 출력
+
+        int ch3 = 67;
+        System.out.println(ch3);        // 문자 정수 값 출력
+        System.out.println((char)ch3);  // 정수 값에 해당하는 문자 출력
+    }
+}
