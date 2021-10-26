8e5dbee3656f0b4ac75a82558af0d29e772dd91b
whitebear05 <woojs0505@naver.com>
2021-10-26 09:16:41 +0900

    Add: Boolean, Double, Type Inference

---
+++ src/chapter2/TypeInference.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class TypeInference {
+    public static void main(String[] args) {
+        var i = 10;         // int i = 10으로 컴파일됨
+        var j = 10.0;       // double j = 10.0으로 컴파일됨
+        var str = "hello";  // String str = "hello"로 컴파일됨
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(str);
+
+        str = "test";
+        // str = 3;
+    }
+}
