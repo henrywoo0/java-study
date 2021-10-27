ce73e993811520eadbbec8de1345f223fa28d000
whitebear05 <woojs0505@naver.com>
2021-10-27 23:51:32 +0900

    Add files

---
+++ src/thisex/CallAnotherConst.java
@@ -0,0 +1,31 @@
+package thisex;
+
+class Person {
+    String name;
+    int age;
+
+    Person() {
+        this("이름 없음", 1);
+    }
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    Person returnItSelf() {
+        return this;
+    }
+}
+
+public class CallAnotherConst {
+    public static void main(String[] args) {
+        Person noName = new Person();
+        System.out.println(noName.name);
+        System.out.println(noName.age);
+
+        Person p = noName.returnItSelf();
+        System.out.println(p);
+        System.out.println(noName);
+    }
+}
