cb8beedb6dcdd3e4f8c5b59231ebcca733350c58
whitebear05 <woojs0505@naver.com>
2021-10-27 10:29:26 +0900

    Add: instance

---
+++ src/classpart/Person.java
@@ -0,0 +1,9 @@
+package classpart;
+
+public class Person {
+    String name;
+    int height;
+    double weight;
+    char gender;
+    boolean married;
+}
---
+++ src/classpart/Student.java
@@ -0,0 +1,16 @@
+package classpart;
+
+public class Student {
+    int studentID;
+    String studentName;
+    int grade;
+    String address;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
+}
