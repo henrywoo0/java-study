ae50705e1ee8892051e5bdc4cb8e78ce99a8f7d0
whitebear05 <woojs0505@naver.com>
2021-10-27 18:04:02 +0900

    Add: constructor

--- src/constructor/PersonTest.java
+++ src/constructor/PersonTest.java
@@ -2,6 +2,11 @@ package constructor;
 
 public class PersonTest {
     public static void main(String[] args) {
-        Person personLee = new Person();
+        Person personKim = new Person();
+        personKim.name = "김경태";
+        personKim.weight = 85.5F;
+        personKim.height = 180.0F;
+
+        Person personLee = new Person("이경태", 175, 55);
     }
 }
