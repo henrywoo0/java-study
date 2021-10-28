b01fc5162fcb3b1be8d99a66dd13506fc6f4e0b4
whitebear05 <woojs0505@naver.com>
2021-10-28 11:54:50 +0900

    Add: singleton pattern

---
+++ src/singleton/CompanyTest.java
@@ -0,0 +1,9 @@
+package singleton;
+
+public class CompanyTest {
+    public static void main(String[] args) {
+        Company myCompany1 = Company.getInstance();
+        Company myCompany2 = Company.getInstance();
+        System.out.println(myCompany1 == myCompany2);
+    }
+}
