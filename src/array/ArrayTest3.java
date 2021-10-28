f2991db80bd8d0f0146f15c3a5b4f14400f7115f
whitebear05 <woojs0505@naver.com>
2021-10-28 17:33:35 +0900

    Add: array

---
+++ src/array/ArrayTest3.java
@@ -0,0 +1,16 @@
+package array;
+
+public class ArrayTest3 {
+    public static void main(String[] args) {
+        double[] data = new double[5];
+        int size = 0;
+
+        data[0] = 10.0; size++;
+        data[1] = 20.0; size++;
+        data[2] = 30.0; size++;
+
+        for(int i = 0; i < size; i++) {
+            System.out.println(data[i]);
+        }
+    }
+}
---
+++ src/array/CharArray.java
@@ -0,0 +1,16 @@
+package array;
+
+public class CharArray {
+    public static void main(String[] args) {
+        char[] alphabets = new char[26];
+        char ch = 'A';
+
+        for(int i = 0; i < alphabets.length; i++, ch++) {
+            alphabets[i] = ch;
+        }
+
+        for(int i = 0; i < alphabets.length; i++) {
+            System.out.println(alphabets[i] + ", " + (int)alphabets[i]);
+        }
+    }
+}
