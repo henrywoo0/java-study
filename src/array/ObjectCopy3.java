bff7b96a42a6cee0b1170548fb070cd1a518d59f
whitebear05 <woojs0505@naver.com>
2021-11-04 23:48:01 +0900

    Add: deep copy

--- src/array/ObjectCopy3.java
+++ src/array/ObjectCopy3.java
@@ -1,7 +1,7 @@
 package array;
 
-// 얕은 복사
-public class ObjectCopy2 {
+// 깊은 복사
+public class ObjectCopy3 {
     public static void main(String[] args) {
         Book[] bookArray1 = new Book[3];
         Book[] bookArray2 = new Book[3];
@@ -9,11 +9,18 @@ public class ObjectCopy2 {
         bookArray1[0] = new Book("태백산맥", "조정래");
         bookArray1[1] = new Book("데미안", "헤르만 헤세");
         bookArray1[2] = new Book("어떻게 살 것인가", "유시민");
-        System.arraycopy(bookArray1, 0, bookArray2, 0, 3);
+
+        bookArray2[0] = new Book();
+        bookArray2[1] = new Book();
+        bookArray2[2] = new Book();
+
+        for(int i = 0; i < bookArray1.length; i++) {
+            bookArray2[i].setBookName(bookArray1[i].getBookName());
+            bookArray2[i].setAuthor(bookArray1[i].getAuthor());
+        }
 
         for(int i = 0; i < bookArray2.length; i++) {
             bookArray2[i].showBookInfo();
-            System.out.println(bookArray2[i]);
         }
 
         bookArray1[0].setBookName("나목");
@@ -25,7 +32,7 @@ public class ObjectCopy2 {
         }
 
         System.out.println("=== bookArray2 ===");
-        for(int i = 0; i < bookArray2.length; i++) {
+        for(int i = 0; i < bookArray1.length; i++) {
             bookArray2[i].showBookInfo();
         }
     }
