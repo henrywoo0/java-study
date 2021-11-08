cfa0272f34ebda3d89ee2eef5394a52916664613
whitebear05 <woojs0505@naver.com>
2021-11-08 18:19:47 +0900

    Add: inheritance

--- src/inheritance/Customer.java
+++ src/inheritance/Customer.java
@@ -8,9 +8,18 @@ public class Customer {
     int bonusPoint; // 보너스 포인트
     double bonusRatio; // 적립 비율
 
-    public Customer() {
-        customerGrade = "SILVER"; // 기본 등급
-        bonusRatio = 0.01; // 보너스 포인트 기본 적립 비율
+//    public Customer() {
+//        customerGrade = "SILVER"; // 기본 등급
+//        bonusRatio = 0.01; // 보너스 포인트 기본 적립 비율
+//        System.out.println("Customer() 생성자 호출");
+//    }
+
+    public Customer(int customerID, String customerName) {
+        this.customerID = customerID;
+        this.customerName = customerName;
+        customerGrade = "SILVER";
+        bonusRatio = 0.01;
+        System.out.println("Customer");
     }
 
     public int calcPrice(int price) {
--- src/inheritance/VIPCustomer.java
+++ src/inheritance/VIPCustomer.java
@@ -4,13 +4,26 @@ public class VIPCustomer extends Customer{
     private int agentID; // VIP 고객 상담원 아이디
     double saleRatio; // 할인율
 
-    public VIPCustomer() {
+    public VIPCustomer(int customerID, String customerName, int agentID) {
+        super(customerID, customerName);
         customerGrade = "VIP";
         bonusRatio = 0.05;
         saleRatio = 0.1;
+        this.agentID = agentID;
+        System.out.println("VIPCustomer(int, String, int) 생성자 호출");
     }
 
     public int getAgentID() {
         return agentID;
+    }
+
+    public String showVIPInfo() {
+        return super.showCustomerInfo() + "담당 상담원 아이디는 " + agentID + "입니다.";
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio; // 보너스 포인트 적립
+        return price - (int)(price * saleRatio); // 할인된 가격을 계산하여 반환
     }
 }
