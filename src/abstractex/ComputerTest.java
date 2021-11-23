56b39f6958b4ee26c0424a2f06694c606a7640a0
whitebear05 <woojs0505@naver.com>
2021-11-23 12:49:29 +0900

    abstract class

---
+++ src/abstractex/ComputerTest.java
@@ -0,0 +1,19 @@
+package abstractex;
+
+public class ComputerTest {
+    public static void main(String[] args) {
+        // Computer c1 = new Computer(); // 오류 발생
+        Computer c2 = new DeskTop();
+        // Computer c3 = new NoteBook(); // 오류 발생
+        Computer c4 = new MyNoteBook();
+
+        c2.turnOn();
+        c4.turnOn();
+
+        c2.display();
+        c4.display();
+
+        c2.typing();
+        c4.typing();
+    }
+}
