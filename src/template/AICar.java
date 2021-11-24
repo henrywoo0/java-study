81115140de016c6d4ee3cfdb3cede648b4dbd570
whitebear05 <woojs0505@naver.com>
2021-11-24 09:18:54 +0900

    template method

---
+++ src/template/AICar.java
@@ -0,0 +1,14 @@
+package template;
+
+public class AICar extends Car {
+    @Override 
+    public void drive() {
+        System.out.println("자율 주행합니다");
+        System.out.println("자동차가 알아서 방향을 전환합니다");
+    }
+    
+    @Override
+    public void stop() {
+        System.out.println("스스로 멈춥니다");
+    }
+}
